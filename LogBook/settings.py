0f16e6ae4e79dff11a9820f60ca3260b72423a43
paul-fitzpatrick <paul.fitzpatrick@irishrail.ie>
2023-02-25 21:00:04 +0000

    secret key #2 commented out

--- LogBook/settings.py
+++ LogBook/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a!4&7noccj53@auuq03gns#wudygla-5i5g5lwlxa7p406e*3v'
+# SECRET_KEY = 'django-insecure-a!4&7noccj53@auuq03gns#wudygla-5i5g5lwlxa7p406e*3v'-----------??
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True  #--------------dev
